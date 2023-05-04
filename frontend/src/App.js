1259f8767ae7400814531364c29614878d7a8060
m-grainger <grainger.pdx@gmail.com>
2023-05-04 14:14:20 -0700

    feat: added basic react frontend with CRA

---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div
+      className="App list-group-item justify-content-center align-items-center mx-auto"
+      style={{ width: "400px", backgroundColor: "white", marginTop: "15px" }}
+    >
+      <h1
+        className="card text-white bg-primary mb-1"
+        styleName="max-width: 20rem;"
+      >
+        Task Manager
+      </h1>
+      <h6 className="card text-white bg-primary mb-3">
+        {" "}
+        FASTAPI - React - MongoDB
+      </h6>
+      <div className="card-body">
+        <h5 className="card text-white bg-dark mb-3">Add your task</h5>
+        <span className="card-text">
+          <input className="mb-2 form-control titleIn" placeholder="Title" />
+          <input
+            className="mb-2 form-control desIn"
+            placeholder="Description"
+          />
+          <button
+            className="btn btn-outline-primary mx-2 mb-3"
+            style={{ borderRadius: "50px", "font-weight": "bold" }}
+          >
+            Add Task
+          </button>
+        </span>
+        <h5 className="card text-white bg-dark mb-3">Your Tasks</h5>
+        <div>{/*external component*/}</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
