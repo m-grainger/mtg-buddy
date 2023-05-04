2424a6f3b84804d61bf4163beb5b6e4b5397a75d
m-grainger <grainger.pdx@gmail.com>
2023-05-04 13:14:32 -0700

    feat: updated main funcs

--- backend/database.py
+++ backend/database.py
@@ -4,23 +4,23 @@ import traceback
 # MongoDB driver
 import motor.motor_asyncio
 
-client = motor.motor.asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
 database = client.TodoList
 collection = database.todo
 
 
-def db_error_handler(error, func_name):
+def db_error_handler(err, func_name):
     print(
-        f"Error was thrown from {func_name} and resulted in error of {error}")
+        f"Error was thrown from {func_name} and resulted in error of {err}")
 
 
 async def fetch_one_todo(title):
     try:
         document = await collection.find_one({"title": title})
-    except Exception as e:
-        tb_str = traceback.extract_tb(e.__traceback__).as_list()
+    except Exception as err:
+        tb_str = traceback.extract_tb(err.__traceback__).as_list()
         func_name = tb_str[-1][2]
-        db_error_handler(e, func_name)
+        db_error_handler(err, func_name)
 
     return document
 
@@ -31,10 +31,10 @@ async def fetch_all_todos():
         cursor = collection.find()
         async for document in cursor:
             todos.append(Todo(**document))
-    except Exception as e:
-        tb_str = traceback.extract_tb(e.__traceback__).as_list()
+    except Exception as err:
+        tb_str = traceback.extract_tb(err.__traceback__).as_list()
         func_name = tb_str[-1][2]
-        db_error_handler(e, func_name)
+        db_error_handler(err, func_name)
     return todos
 
 
@@ -42,10 +42,10 @@ async def create_todo(todo):
     document = todo
     try:
         result = await collection.insert_one(document)
-    except Exception as e:
-        tb_str = traceback.extract_tb(e.__traceback__).as_list()
+    except Exception as err:
+        tb_str = traceback.extract_tb(err.__traceback__).as_list()
         func_name = tb_str[-1][2]
-        db_error_handler(e, func_name)
+        db_error_handler(err, func_name)
 
     return result
 
@@ -54,18 +54,18 @@ async def update_todo(title, description):
     try:
         await collection.update_one({"title": title}, {"$set": {"description": description}})
         document = await collection.find_one({"title": title})
-    except Exception as e:
-        tb_str = traceback.extract_tb(e.__traceback__).as_list()
+    except Exception as err:
+        tb_str = traceback.extract_tb(err.__traceback__).as_list()
         func_name = tb_str[-1][2]
-        db_error_handler(e, func_name)
+        db_error_handler(err, func_name)
     return document
 
 
 async def remove_todo(title):
     try:
         await collection.delete_one({"title": title})
-    except Exception as e:
-        tb_str = traceback.extract_tb(e.__traceback__).as_list()
+    except Exception as err:
+        tb_str = traceback.extract_tb(err.__traceback__).as_list()
         func_name = tb_str[-1][2]
-        db_error_handler(e, func_name)
+        db_error_handler(err, func_name)
     return True
