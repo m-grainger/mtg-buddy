b4754ccf28082a3c86fb27218759bbfd1a7a94f6
m-grainger <grainger.pdx@gmail.com>
2023-05-05 09:09:43 -0700

    fix: cleaned up the code and fixed bug where create call was failing

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,8 @@
-from model import Todo
-import traceback
-
-# MongoDB driver
 import motor.motor_asyncio
+import traceback
+from model import Todo
 
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
 database = client.TodoList
 collection = database.todo
 
@@ -17,55 +15,53 @@ def db_error_handler(err, func_name):
 async def fetch_one_todo(title):
     try:
         document = await collection.find_one({"title": title})
+        return document
     except Exception as err:
         tb_str = traceback.extract_tb(err.__traceback__).as_list()
         func_name = tb_str[-1][2]
         db_error_handler(err, func_name)
-
-    return document
 
 
 async def fetch_all_todos():
     todos = []
     try:
-        cursor = collection.find()
+        cursor = collection.find({})
         async for document in cursor:
             todos.append(Todo(**document))
+        return todos
     except Exception as err:
         tb_str = traceback.extract_tb(err.__traceback__).as_list()
         func_name = tb_str[-1][2]
         db_error_handler(err, func_name)
-    return todos
 
 
 async def create_todo(todo):
-    document = todo
     try:
+        document = todo
         result = await collection.insert_one(document)
+        return document
     except Exception as err:
         tb_str = traceback.extract_tb(err.__traceback__).as_list()
         func_name = tb_str[-1][2]
         db_error_handler(err, func_name)
 
-    return document
 
-
-async def update_todo(title, description):
+async def update_todo(title, desc):
     try:
-        await collection.update_one({"title": title}, {"$set": {"description": description}})
+        await collection.update_one({"title": title}, {"$set": {"description": desc}})
         document = await collection.find_one({"title": title})
+        return document
     except Exception as err:
         tb_str = traceback.extract_tb(err.__traceback__).as_list()
         func_name = tb_str[-1][2]
         db_error_handler(err, func_name)
-    return document
 
 
 async def remove_todo(title):
     try:
         await collection.delete_one({"title": title})
+        return True
     except Exception as err:
         tb_str = traceback.extract_tb(err.__traceback__).as_list()
         func_name = tb_str[-1][2]
         db_error_handler(err, func_name)
-    return True
--- backend/main.py
+++ backend/main.py
@@ -1,33 +1,36 @@
+from fastapi import FastAPI, HTTPException
+
+from model import Todo
+
 from database import (
     fetch_one_todo,
     fetch_all_todos,
     create_todo,
     update_todo,
-    remove_todo
+    remove_todo,
 )
-from model import Todo
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
 
+
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
 
-# port 3000 for react
-origins = ['http://localhost:3000']
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="True",
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
-
-# routes
 
 
 @app.get("/")
-def read_root():
-    return {"Ping": "Pong"}
+async def read_root():
+    return {"Hello": "World"}
 
 
 @app.get("/api/todo")
@@ -36,37 +39,33 @@ async def get_todo():
     return response
 
 
-@app.get("/api/todo{title}", response_model=Todo)
-async def get_todo_by_id(title: str):
+@app.get("/api/todo/{title}", response_model=Todo)
+async def get_todo_by_title(title):
     response = await fetch_one_todo(title)
     if response:
         return response
-    else:
-        raise HTTPException(404, f"there is no TODO item with title: {title}")
+    raise HTTPException(404, f"There is no todo with the title {title}")
 
 
-@app.post("/api/todo", response_model=Todo)
+@app.post("/api/todo/", response_model=Todo)
 async def post_todo(todo: Todo):
     response = await create_todo(todo.dict())
     if response:
         return response
-    else:
-        raise HTTPException(400, f"bad request creating {todo.title}")
+    raise HTTPException(400, "Something went wrong")
 
 
-@app.put("/api/todo{title}", response_model=Todo)
-async def put_todo(title: str, description: str):
-    response = await update_todo(title, description)
+@app.put("/api/todo/{title}/", response_model=Todo)
+async def put_todo(title: str, desc: str):
+    response = await update_todo(title, desc)
     if response:
         return response
-    else:
-        raise HTTPException(404, f"unable to update {title}")
+    raise HTTPException(404, f"There is no todo with the title {title}")
 
 
-@app.delete("/api/todo{title}")
-async def delete_todo(title: str):
+@app.delete("/api/todo/{title}")
+async def delete_todo(title):
     response = await remove_todo(title)
     if response:
-        return "Successfully deleted todo item"
-    else:
-        raise HTTPException(404, f"there was an issue removing {title}")
+        return "Successfully deleted todo"
+    raise HTTPException(404, f"There is no todo with the title {title}")
