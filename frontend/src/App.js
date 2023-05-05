01184ca9e0f7994895589879ecde27bd1e6b0661
m-grainger <grainger.pdx@gmail.com>
2023-05-05 10:33:10 -0700

    chore: cleanup and removal of old code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,36 +26,26 @@ function App() {
   };
 
   return (
-    <div class="App list-group-item flex justify-center items-center mx-auto w-96 bg-white mt-4">
-      <h6 class="card text-white bg-primary mb-3">FASTAPI - React - MongoDB</h6>
-      <div class="card-body">
-        <h5 class="card text-white bg-dark mb-3">Add Your Task</h5>
-        <span class="card-text">
-          <input
-            class="mb-2 form-control titleIn"
-            onChange={(event) => setTitle(event.target.value)}
-            placeholder="Title"
-          />
-          <input
-            class="mb-2 form-control desIn"
-            onChange={(event) => setDesc(event.target.value)}
-            placeholder="Description"
-          />
-          <button
-            class="btn btn-outline-primary mx-2 mb-3 rounded-full font-bold"
-            onClick={addTodoHandler}
-          >
-            Add Task
-          </button>
-        </span>
-        <h5 class="card text-white bg-dark mb-3">Your Tasks</h5>
-        <div>
-          <TodoView todoList={todoList} />
+    <div class="flex min-h-screen bg-gradient-to-b from-purple-500 to-blue-500">
+      <div class="absolute top-0 left-0 mt-4 ml-20">
+        <input
+          type="text"
+          placeholder="Search"
+          class="mt-5 ml-4 px-4 py-2 rounded-lg border border-gray-400 focus:outline-none focus:border-gray-500"
+        />
+        <button
+          type="submit"
+          class="ml-2 px-4 py-2 rounded-lg bg-blue-500 hover:bg-blue-600 text-white font-bold"
+        >
+          Submit
+        </button>
+      </div>
+      <div class="h-screen w-screen flex justify-center items-center mt-10">
+        <div class="w-[90%] h-[90%] flex justify-center items-center bg-pink-300 bg-opacity-20 rounded-lg p-10">
+          <div class="w-3/5 h-full border-black border-2 rounded-lg mr-4"></div>
+          <div class="w-2/5 h-full border-black border-2 rounded-lg"></div>
         </div>
       </div>
-      <h6 class="card text-dark bg-warning py-1 mb-0">
-        Copyright 2021, All rights reserved &copy;
-      </h6>
     </div>
   );
 }
